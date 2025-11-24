113ebc37af0427da89f7169f1f4fd634cfa86cca
j-diefenbach <jamescdiefenbach@gmail.com>
2025-11-24 11:06:20 +1100

    better resume download buttons

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -12,42 +12,77 @@ import Testimonial from "../testimonial";
 import PopularClients from "../popular-clients";
 import ContactForm from "../contact-form";
 import React from "react";
-import { Button, Divider, Stack } from "@mui/material";
+import { Box, Button, Divider, Modal, Stack } from "@mui/material";
 import { Download } from "@mui/icons-material";
 
 export default function Portfolio() {
     const resumeUrl = "/2025_Resume.pdf";
     const cvUrl = "/2025_CV.pdf";
     const [displayItem, setDisplayItem] = React.useState(resumeUrl);
+    const isSmall = typeof window !== "undefined" ? window.innerWidth < 1024 : false;
+    const [modalOpen, setModalOpen] = React.useState(false);
   return (
     <>
       <Navbar />
-    <Stack direction="row" spacing={5} className="justify-center my-4">
+    <Stack direction="row" spacing={isSmall ? 0: 5} className="justify-center my-4">
         <Button variant="outlined" className="flex items-center gap-2"
+        style = {{ fontSize: isSmall ? "8px" : "16px" }}
         onClick={() => setDisplayItem(resumeUrl)}
         >
             Resume
         </Button>
         <Button variant="outlined" className="flex items-center gap-2"
+        style = {{ fontSize: isSmall ? "8px" : "16px" }}
         onClick={() => setDisplayItem(cvUrl)}
         >
             CV
         </Button>
         <Divider orientation="vertical" flexItem />
-        <Button variant="outlined" className="flex items-center gap-2"
+        <Button variant="outlined" className="flex items-center"
+        style = {{ fontSize: isSmall ? "8px" : "16px" }}
         startIcon={<Download />}
-        onClick={() => setDisplayItem("/2025_Resume.docx")}
+        onClick={() => setModalOpen(true)}
         >
-            Resume (.docx)
         </Button>
-        <Button variant="outlined" className="flex items-center gap-2"
-        startIcon={<Download />}
-        onClick={() => setDisplayItem("/2025_CV.docx")}
-        >
-            CV (.docx)
-        </Button>
+        <Modal open={modalOpen} onClose={() => setModalOpen(false)}>
+            <Box sx={{ position: 'absolute' as 'absolute', top: '50%', left: '30%', transform: 'translate(-12.5%, -50%)', bgcolor: 'background.paper', border: '2px solid #000', boxShadow: 24, p: 4, }}>
+                <Stack>
+                    <Button variant="contained" className="flex items-center gap-2"
+                    style = {{ fontSize: isSmall ? "12px" : "16px" }}
+                    href={displayItem} download
+                    >
+                        Resume PDF
+                    </Button>
+                    <Button variant="contained" className="flex items-center gap-2"
+                    style = {{ fontSize: isSmall ? "12px" : "16px" }}
+                    href={"/2025_Resume.docx"} download
+                    >
+                        Resume .docx
+                    </Button>
+                    <Button variant="contained" className="flex items-center gap-2"
+                    style = {{ fontSize: isSmall ? "12px" : "16px" }}
+                    href={cvUrl} download
+                    >
+                        CV PDF
+                    </Button>
+                    <Button variant="contained" className="flex items-center gap-2"
+                    style = {{ fontSize: isSmall ? "12px" : "16px" }}
+                    href={"/2025_CV.docx"} download
+                    >
+                        CV .docx
+                    </Button>
+                    <Button variant="text" className="flex items-center justify-end"
+                    style = {{ fontSize: isSmall ? "12px" : "16px" }}
+                    onClick={() => setModalOpen(false)}
+                    >
+                        Close
+                    </Button>
+                </Stack>
+            </Box>
+            
+        </Modal>
     </Stack>
-    <section className="w-full h-[90vh]">
+    <section className="w-full h-full">
         <object data={displayItem} type="application/pdf" width="100%" height="100%">
             <iframe src={displayItem} width="100%" height="100%" title="Resume PDF" />
         </object>
